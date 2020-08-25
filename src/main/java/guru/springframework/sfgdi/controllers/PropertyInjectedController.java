8144321781e775b38fe22411f057ac57fffa6515
SongHyunKim <zuppiy@naver.com>
2020-08-25 10:31:55 +0900

    feat: ⚡ @Service annotation을 추가해서 이게 스프링 component 인것과 PropertyInjectedController, SetterInjectedController, ConstructorInjectedController 추가

--- src/main/java/guru/springframework/sfgdi/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PropertyInjectedController.java
@@ -2,9 +2,13 @@ package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
 import guru.springframework.sfgdi.services.GreetingServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class PropertyInjectedController{
 
+    @Autowired
     public GreetingService greetingService;
 
     public String getGreeting(){
